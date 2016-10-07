dd2816b4ce54e858158b6ec3133cfb338c13d6ce
pikec <pikec@oregonstate.edu>
2016-10-07 12:14:01 -0500

    Add files via upload

---
+++ database/type_and_class.sql
@@ -0,0 +1,12 @@
+INSERT INTO `act_type` (`title`) VALUES ('regular');
+
+INSERT INTO `act_type` (`title`) VALUES ('admin');
+
+INSERT INTO `class` (`title`) VALUES ('Employee of the Week');
+
+INSERT INTO `class` (`title`) VALUES ('Employee of the Month');
+
+INSERT INTO `class` (`title`) VALUES ('Employee of the Quarter');
+
+INSERT INTO `class` (`title`) VALUES ('Employee of the Year');
+

eef0a8aaed1a861017874d10e962d3548f83bc1f
Kyle Bedell <kyle.bedell@gmail.com>
2016-10-30 10:38:01 -0400

    UsersXRegion finished

---
+++ site/analytics/getUsersXRegion.php
@@ -0,0 +1,35 @@
+<?php 
+
+require_once("../dbconfig.php");
+
+//Turn on error reporting
+ini_set('display_errors', 'On');
+
+//Connects to the database
+$mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_DB);
+if($mysqli->connect_errno){
+	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+
+$result = mysqli_query($mysqli, "SELECT AU.state, COUNT(AU.email) AS 'UserCount' FROM award_user AU GROUP BY AU.state ORDER BY AU.state;");
+$output = ["cols" => [
+	["id" => "", "label" => "State", "pattern" => "", "type" => "string"],
+	["id" => "", "label" => "Users", "pattern" => "", "type" => "number"],
+]];
+$rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+	$item = array();
+	 foreach ($r as $key => $value) {
+		 if ($key === 'UserCount') {
+			 $item[] = ["v" => intval($value), "f" => null];;
+		 } else {
+			$item[] = ["v" => $value, "f" => null];
+		 }
+	 }
+	 $rows[] = ["c" => $item ];
+ }
+$output["rows"] = $rows;
+print json_encode($output);
+$result->close();
+
+?>
---
+++ site/analytics/usersXregion.php
@@ -0,0 +1,89 @@
+<!--Load the AJAX API-->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+
+  // Load the Visualization API and the controls package.
+  google.charts.load('current', {'packages':['geochart', 'controls']});
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawDashboard);
+
+  // Callback that creates and populates a data table,
+  // instantiates a dashboard, a range slider and a pie chart,
+  // passes in the data and draws it.
+  function drawDashboard() {
+
+    // Prepare the data
+    var jsonData = $.ajax({
+        url: "analytics/getUsersXRegion.php",
+        dataType: "json",
+        async: false
+        }).responseText;
+    
+    var data = new google.visualization.DataTable(jsonData);
+
+    // Create a dashboard.
+    var dashboard = new google.visualization.Dashboard(
+        document.getElementById('dashboard_div'));
+
+    // Create a range slider, passing some options
+    var stateFilter = new google.visualization.ControlWrapper({
+      'controlType': 'CategoryFilter',
+      'containerId': 'filter_div1',
+      'options': {
+        'filterColumnLabel': 'State',
+        'filterColumnIndex': 0,
+            'ui': {
+              'caption': 'Choose a state ...',
+              'allowTyping': false,
+              'selectedValuesLayout': 'below',
+              // 'cssClass': 'form-control',
+            }
+      }
+    });
+
+    // Create a pie chart, passing some options
+    var chart = new google.visualization.ChartWrapper({
+      'chartType': 'GeoChart',
+      'containerId': 'chart_div',
+      'options': {
+        'region': 'US',
+        'resolution': 'provinces',
+        'height': 400,
+        'colorAxis': {'colors': ['#F0CB35', '#C02425']},
+      }
+    });
+    
+    // Options for table
+    var options = {
+      width: '100%', 
+      page: 'enable',
+      pageSize: 5,
+      allowHtml: true,
+    };
+    
+    var table = new google.visualization.ChartWrapper({
+        chartType: 'Table',
+        containerId: 'table_div',
+        options,
+    });
+
+    // Create a range slider, passing some options
+    var awardsRangeSlider = new google.visualization.ControlWrapper({
+      'controlType': 'NumberRangeFilter',
+      'containerId': 'filter_div2',
+      'options': {
+        'filterColumnLabel': 'Users'
+      }
+    });
+
+    // Establish dependencies, declaring that 'filter' drives 'chart',
+    // so that the pie chart will only display entries that are let through
+    // given the chosen slider range.
+    dashboard.bind([awardsRangeSlider, stateFilter], table);
+    dashboard.bind([awardsRangeSlider, stateFilter], chart);
+
+    // Draw the dashboard.
+    dashboard.draw(data);
+  }
+</script>

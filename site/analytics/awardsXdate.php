2ab649dcab867d3b77e94c871bab4a4cf580c79c
Kyle Bedell <kyle.bedell@gmail.com>
2016-10-30 10:21:15 -0400

    Rough draft AwardsXDate

--- site/analytics/awardsXdate.php
+++ site/analytics/awardsXdate.php
@@ -1,59 +1,84 @@
-<?php
-function makeChart($first, $second, $mysqli) {
-  $data = [];
-?>
-
+<!--Load the AJAX API-->
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
-google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawBasic);
 
-function drawBasic() {
+  // Load the Visualization API and the controls package.
+  google.charts.load('current', {'packages':['corechart', 'controls']});
 
-      var data = new google.visualization.DataTable();
-      data.addColumn('date', '<?php echo $second ?>');
-      data.addColumn('number', '<?php echo $first ?>');
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawDashboard);
 
-      data.addRows([
-        <?php
-        if(!($stmt = $mysqli->prepare("SELECT A.award_date, COUNT(A.class_id) as 'totalAwards' FROM award_user AU INNER JOIN award A ON A.user_id = AU.id GROUP BY A.award_date LIMIT 15;"))){
-          echo "Prepare failed: " . $stmt->errno . " " . $stmt->error;
-        }
-        if(!$stmt->execute()){
-          echo "Execute failed: " . $mysqli->connect_errno . " " . $mysqli->connect_error;
-        }
-        if(!$stmt->bind_result($date, $awards)){
-          echo "Bind failed: " . $mysqli->connect_errno . " " . $mysqli->connect_error;
-        }
-        while($stmt->fetch()){
-          $dateParts = explode("-", $date);
-          echo "[new Date(" . $dateParts[0] . ", " . $dateParts[1] . ", " . $dateParts[2] . "), " . $awards . "],";
-        }
-        $stmt->close();
-        ?>
-      ]);
+  // Callback that creates and populates a data table,
+  // instantiates a dashboard, a range slider and a pie chart,
+  // passes in the data and draws it.
+  function drawDashboard() {
 
-      var options = {
-      legend: { position: 'none' },
-      hAxis: {
-        title: 'Date',
+    // Prepare the data
+    var jsonData = $.ajax({
+        url: "analytics/getAwardsXDate.php",
+        dataType: "json",
+        async: false
+        }).responseText;
+    
+    var data = new google.visualization.DataTable(jsonData);
+
+    // Create a dashboard.
+    var dashboard = new google.visualization.Dashboard(
+        document.getElementById('dashboard_div'));
+
+
+    var chart = new google.visualization.ChartWrapper({
+      'chartType': 'LineChart',
+      'containerId': 'chart_div',
+      'options': {
+        'legend': {'position': 'none'},
+        'height': 400,
       },
-      vAxis: {
-        title: 'Awards',
-      },
-      colors: ['#F0CB35']
-      };
+      // 'view': {'columns': [2, 4]}, // TODO
+    });
+    
+    // Options for table
+    var options = {
+      width: '100%', 
+      page: 'enable',
+      pageSize: 5,
+      allowHtml: true,
+    };
+    
+    var table = new google.visualization.ChartWrapper({
+        chartType: 'Table',
+        containerId: 'table_div',
+        options,
+        // 'view': {'columns': [0, 1, 3, 4]},
+    });
 
-      var chart = new google.visualization.ColumnChart(
-        document.getElementById('container_div'));
+    // Create a range slider, passing some options
+    var awardsRangeSlider = new google.visualization.ControlWrapper({
+      'controlType': 'NumberRangeFilter',
+      'containerId': 'filter_div3',
+      'options': {
+        'filterColumnLabel': 'Awards'
+      }
+    });
+    
+    var dateRangeSlider = new google.visualization.ControlWrapper({
+      'controlType': 'DateRangeFilter',
+      'containerId': 'filter_div1',
+      'options': {
+        'filterColumnLabel': 'Date'
+        // 'ui': {
+        //     format: 'F jS, Y',
+        // }
+      }
+    });
 
-      chart.draw(data, options);
-    }
-  
-  var chart = new google.visualization.BarChart(container);
-  </script>
-<?php
-}
-function makeTable($first, $second, $mysqli) {
-}
-?>
+    // Establish dependencies, declaring that 'filter' drives 'chart',
+    // so that the pie chart will only display entries that are let through
+    // given the chosen slider range.
+    dashboard.bind([awardsRangeSlider, dateRangeSlider], table);
+    dashboard.bind([awardsRangeSlider, dateRangeSlider], chart);
+
+    // Draw the dashboard.
+    dashboard.draw(data);
+  }
+</script>
---
+++ site/analytics/getAwardsXDate.php
@@ -0,0 +1,38 @@
+<?php 
+
+require_once("../dbconfig.php");
+
+//Turn on error reporting
+ini_set('display_errors', 'On');
+
+//Connects to the database
+$mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_DB);
+if($mysqli->connect_errno){
+	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+
+$result = mysqli_query($mysqli, "SELECT A.award_date, COUNT(A.class_id) as 'totalAwards' FROM award_user AU INNER JOIN award A ON A.user_id = AU.id GROUP BY A.award_date;");
+$output = ["cols" => [
+	["id" => "", "label" => "Date", "pattern" => "", "type" => "date"],
+	["id" => "", "label" => "Awards", "pattern" => "", "type" => "number"],
+]];
+$rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+	$item = array();
+	 foreach ($r as $key => $value) {
+		 if ($key === 'totalAwards') {
+			$item[] = ["v" => intval($value), "f" => null];;
+		 } else {
+			 $dateparts = explode("-", $value);
+			 $date = date_create($value);
+			 
+			$item[] = ["v" => "Date($dateparts[0], $dateparts[1], $dateparts[2])", "f" => date_format($date, 'F jS, Y')];
+		 }
+	 }
+	 $rows[] = ["c" => $item ];
+ }
+$output["rows"] = $rows;
+print json_encode($output);
+$result->close();
+
+?>
